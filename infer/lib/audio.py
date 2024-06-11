a5992a8fcc7ddbad927dfbe9289b5cc51150c7fd
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-06-11 10:49:30 +0700

    fix: default to the user-requested format in case of hashing failure

--- infer/lib/audio.py
+++ infer/lib/audio.py
@@ -16,9 +16,9 @@ audio_format_dict: Dict[str, str] = {
 
 def wav2(i: BytesIO, o: BufferedWriter, format: str):
     inp = av.open(i, "r")
-    format = video_format_dict.get(format, "mp4")
+    format = video_format_dict.get(format, format)
     out = av.open(o, "w", format=format)
-    format = audio_format_dict.get(format, "aac")
+    format = audio_format_dict.get(format, format)
 
     ostream = out.add_stream(format)
 
