d2a06db727e1b3d7fbbc3f64f66af7c6a6de7d9a
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-06-13 07:30:49 +0700

    refactor(scripts): apply a lot more of refactoring of run script

--- run.sh
+++ run.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 
+set -fa
+
+# Check if Python is installed
+if ! command -v python; then
+  echo "Python not found. Please install Python using your package manager or via PyEnv."
+  exit 1
+fi
+
+requirements_file="requirements/main.txt"
+venv_path=".venv"
+
+# macOS specific environment setup
 if [[ "$(uname)" = "Darwin" ]]; then
-  # macOS specific env:
   export PYTORCH_ENABLE_MPS_FALLBACK=1
   export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
 fi
 
-if [[ -d ".venv" ]]; then
-  echo "Activating venv..."
-  source .venv/bin/activate
-else
+if [[ ! -d "${venv_path}" ]]; then
   echo "Creating venv..."
-  requirements_file="requirements/main.txt"
 
-  # Check if Python is installed
-  if ! command -v python; then
-    echo "Python not found. Please install Python using your package manager or via PyEnv."
-  fi
+  python -m venv "${venv_path}"
+  source "${venv_path}/bin/activate"
 
-  python -m venv .venv
-  source .venv/bin/activate
-
-  # Check if required packages are up-to-date and update them if not
+  # Check if required packages are up-to-date
   pip install --upgrade -r "${requirements_file}"
 fi
+echo "Activating venv..."
+source "${venv_path}/bin/activate"
 
 # Run the main script
 python web.py --pycmd python3
