357a43a3fa18aaad7840a2a9510aa53d1705d41f
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-06-13 19:50:58 +0700

    fix(scripts): remove env variable setting for macOS systems
    use --pycmd python instead of --pycmd python3

--- run.sh
+++ run.sh
@@ -11,12 +11,6 @@ fi
 requirements_file="requirements/main.txt"
 venv_path=".venv"
 
-# macOS specific environment setup
-if [[ "$(uname)" = "Darwin" ]]; then
-  export PYTORCH_ENABLE_MPS_FALLBACK=1
-  export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
-fi
-
 if [[ ! -d "${venv_path}" ]]; then
   echo "Creating venv..."
 
@@ -30,4 +24,4 @@ echo "Activating venv..."
 source "${venv_path}/bin/activate"
 
 # Run the main script
-python web.py --pycmd python3
+python web.py --pycmd python
