168616517a7a382254164d59c885ba9454cc3cd8
tkyaji <tkyaji@gmail.com>
2024-06-26 21:59:55 +0900

    chore: bump librosa to version 0.10.2
    There is a bug in librosa 0.9.1.
    https://github.com/librosa/librosa/pull/1594
    
    As a result, an error occurs when executing the "Vocals/Accompaniment Separation & Reverberation Removal" function.
    
    To address this issue, librosa has been upgraded to version 0.10.2.
    Additionally, torchcrepe has been upgraded due to its dependency on librosa.

--- infer/lib/uvr5_pack/lib_v5/spec_utils.py
+++ infer/lib/uvr5_pack/lib_v5/spec_utils.py
@@ -41,8 +41,8 @@ def wave_to_spectrogram(
         wave_left = np.asfortranarray(wave[0])
         wave_right = np.asfortranarray(wave[1])
 
-    spec_left = librosa.stft(wave_left, n_fft, hop_length=hop_length)
-    spec_right = librosa.stft(wave_right, n_fft, hop_length=hop_length)
+    spec_left = librosa.stft(wave_left, n_fft=n_fft, hop_length=hop_length)
+    spec_right = librosa.stft(wave_right, n_fft=n_fft, hop_length=hop_length)
 
     spec = np.asfortranarray([spec_left, spec_right])
 
@@ -76,7 +76,7 @@ def wave_to_spectrogram_mt(
         kwargs={"y": wave_left, "n_fft": n_fft, "hop_length": hop_length},
     )
     thread.start()
-    spec_right = librosa.stft(wave_right, n_fft, hop_length=hop_length)
+    spec_right = librosa.stft(wave_right, n_fft=n_fft, hop_length=hop_length)
     thread.join()
 
     spec = np.asfortranarray([spec_left, spec_right])
@@ -228,26 +228,30 @@ def cache_or_load(mix_path, inst_path, mp):
 
             if d == len(mp.param["band"]):  # high-end band
                 X_wave[d], _ = librosa.load(
-                    mix_path, bp["sr"], False, dtype=np.float32, res_type=bp["res_type"]
+                    mix_path,
+                    sr=bp["sr"],
+                    mono=False,
+                    dtype=np.float32,
+                    res_type=bp["res_type"]
                 )
                 y_wave[d], _ = librosa.load(
                     inst_path,
-                    bp["sr"],
-                    False,
+                    sr=bp["sr"],
+                    mono=False,
                     dtype=np.float32,
                     res_type=bp["res_type"],
                 )
             else:  # lower bands
                 X_wave[d] = librosa.resample(
                     X_wave[d + 1],
-                    mp.param["band"][d + 1]["sr"],
-                    bp["sr"],
+                    orig_sr=mp.param["band"][d + 1]["sr"],
+                    target_sr=bp["sr"],
                     res_type=bp["res_type"],
                 )
                 y_wave[d] = librosa.resample(
                     y_wave[d + 1],
-                    mp.param["band"][d + 1]["sr"],
-                    bp["sr"],
+                    orig_sr=mp.param["band"][d + 1]["sr"],
+                    target_sr=bp["sr"],
                     res_type=bp["res_type"],
                 )
 
@@ -399,8 +403,8 @@ def cmb_spectrogram_to_wave(spec_m, mp, extra_bins_h=None, extra_bins=None):
                         mp.param["mid_side_b2"],
                         mp.param["reverse"],
                     ),
-                    bp["sr"],
-                    sr,
+                    orig_sr=bp["sr"],
+                    target_sr=sr,
                     res_type="sinc_fastest",
                 )
             else:  # mid
@@ -417,7 +421,7 @@ def cmb_spectrogram_to_wave(spec_m, mp, extra_bins_h=None, extra_bins=None):
                     ),
                 )
                 # wave = librosa.core.resample(wave2, bp['sr'], sr, res_type="sinc_fastest")
-                wave = librosa.core.resample(wave2, bp["sr"], sr, res_type="scipy")
+                wave = librosa.resample(wave2, orig_sr=bp["sr"], target_sr=sr, res_type="scipy")
 
     return wave.T
 
@@ -504,8 +508,8 @@ def ensembling(a, specs):
 def stft(wave, nfft, hl):
     wave_left = np.asfortranarray(wave[0])
     wave_right = np.asfortranarray(wave[1])
-    spec_left = librosa.stft(wave_left, nfft, hop_length=hl)
-    spec_right = librosa.stft(wave_right, nfft, hop_length=hl)
+    spec_left = librosa.stft(wave_left, n_fft=nfft, hop_length=hl)
+    spec_right = librosa.stft(wave_right, n_fft=nfft, hop_length=hl)
     spec = np.asfortranarray([spec_left, spec_right])
 
     return spec
